d6eca17a93ff3539319b157404a514b953b03027
Javi <javier193061@gmail.com>
2022-05-02 18:23:55 -0400

    .

--- src/models/server.js
+++ src/models/server.js
@@ -52,7 +52,7 @@ class Server {
 
     async execute(){
         try {
-            await sequelize.sync({ force: false });
+            await sequelize.sync({ force: true });
             console.log('Connection has been established successfully.');
           } catch (error) {
             console.error('Unable to connect to the database:', error);
